ce3258e0aa1f3514f38fe516eae5f1404ea470b0
TBSTheMountainsSay <blackswadow@yandex.ru>
2023-08-18 13:15:59 +0400

    gh-pages

--- src/features/Main/Main.jsx
+++ src/features/Main/Main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Menu from '../Menu/Menu';
 import Content from '../Content/Content';
+import styles from './Main.module.scss';
 
 const Main = ({}) => {
   return (
-    <div>
+    <div className={styles.main}>
       <Menu />
       <Content />
     </div>
